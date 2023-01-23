788eb0eac088a5bd0fc1ad63545be27fa15dafc0
Su Mine <minemisu@gmail.com>
2023-01-23 20:35:27 +0300

    first commit

--- Extra_exercises.py
+++ Extra_exercises.py
@@ -47,3 +47,64 @@ if year%4==0 and year%100==0 and year%400==0:
     leap = True
 else: leap=False
 print((f"is the year {year} a leap year? \n {leap} "))
+
+## ITERATIONS
+# Exercise 1
+# Calculate the sum of the first 500 natural numbers (from 0 to 500 excluded)
+total_500= 0
+for i in range(0,500):
+    total_500+=i
+print(total_500)
+## Exercise 2
+# Write a program that prints the length of the strings supplied by the user, until the user enters the string 'exit'
+while True:
+    string=input("enter a string : ").lower()
+    if string == 'exit':
+        break
+    else:
+        print(str(len(string)))
+## Create a list containing 20 random integers in (0, 100) (0 included), print the number of even elements and the number of elements greater than 70.
+import random
+printable=[]
+number_of=0
+a_list=[random.randint(0,100) for _ in range(0,20)]
+for i in a_list:   
+    if i%2==0:
+        printable.append(i)
+    if i>70:
+        number_of+=1
+print(f"the even numbers in the list are : {printable}" , f"and the number of elements greater than 70 are : {number_of}")
+
+##  Exercise 4
+# Sort the elements in the guest list. Then, sort it in the opposite order.
+# guests = ['James', 'Mary', 'John', 'Patricia', 'Robert', 'Jennifer'
+guests = ['James', 'Mary', 'John', 'Patricia', 'Robert', 'Jennifer']
+sorted_guests=guests.sort()
+sorted_guests=guests.sort(reverse=True)
+print(guests)
+
+## Exercise 1
+# Write a function to determine the highest number in a list of integer (do not use the max() function).
+import random
+new_list=[random.randint(0,100) for _ in range(0,30)]
+max=0
+for i in range(len(new_list)):
+    if new_list[i]> max
+        max=new_list[i]
+print(f"the highest number of the list is {max}")
+print(f"the whole list is : {new_list}") 
+
+## Exercise 2
+# Write a function that given a list of integers, create another list containing only the elements of even position of the first list.
+# will write on top of last exercises list "new_list"
+even_list=[]
+for i in new_list:
+    if i%2==0:
+        even_list.append(i)
+print(f"the list of even numbers in previous list is: {even_list}")
+
+## Exercise 3
+# Write a lambda function that takes x as a parameter and returns x+2. Then assign it to a variable named L.
+x=0
+L=lambda x : x+2
+print(L(x=10))
