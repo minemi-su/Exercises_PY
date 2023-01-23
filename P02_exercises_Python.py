c803163a2770167643cf6407a0f45e2212048820
Su Mine <minemisu@gmail.com>
2023-01-24 00:10:44 +0300

    added numbers

--- P02_exercises_Python.py
+++ P02_exercises_Python.py
@@ -1,4 +1,4 @@
-## Iterators - While Loop
+## 01 Iterators - While Loop
 # Create ` * ** 
 # the py file inside the exercise is below, but the website it says "Create ` * **" they are side by side, will write that version also 
 # ver01
@@ -14,7 +14,7 @@ while i<6:
     i = i + 1
 print(result)
 
-## Iterators - For Loop
+## 02 Iterators - For Loop
 # Create
 # *
 # ***
@@ -25,12 +25,20 @@ for i in range(1,6):
         continue
     else: print("*"*i)
 
-## Iterators - For Loop
+## 03 Iterators - For Loop
 # Check if it is time for coffee break if it is just break
-# To do that lets iterate through given list don't change the COFFEE BREAK statement.
-# just find another way to do it
+# To do that lets iterate through given list don't change the COFFEE BREAK statement. just find another way to do it 
+# I'm not sure that I understood this but here is a solution acoording to hints
 todo = ["exercise1", "exercise2", "exercise3","coffee break", "exercise4","exercise5","exercise6"]
 for x in todo:
     if x.upper() == "COFFEE BREAK":
         print(x)
         break
+
+## 04 Iterators - For Loop
+# Iterate each elements of list1,tuple1,set1 and print them out 
+# For the dict1 iterate all elements but only print the ones who are living on land in the form of x lives in y
+list1 = ["lion", "monkey", "dog","fish"]
+tuple1 = ("lion", "monkey", "dog","fish")
+set1 = {"lion", "monkey", "dog","fish"}
+dict1 = {"lion":"land", "monkey":"land", "dog":"land","fish":"water"}
